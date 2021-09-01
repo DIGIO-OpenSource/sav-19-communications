d6844f273556ce19566a089318032d6cb7b3948e
Nestor Jodar <nestor@digio.es>
2021-09-01 15:34:52 +0200

    Send command call

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,15 @@
 import axios from 'axios';
 import Parameters from './api/parameters'
+import Commands from './api/commands'
 
 export default class SavRestApi {
 
   parameters: Parameters
+  commands: Commands
   constructor(baseUrl: string){
     axios.defaults.baseURL = baseUrl
 
     this.parameters = new Parameters()
+    this.commands = new Commands()
   }
 }
